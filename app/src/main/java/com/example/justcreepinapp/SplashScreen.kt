657da5681b6781f349cc3fd1c006c74d87dfa935
Justin <blizzorbsorc@gmail.com>
2025-12-08 01:51:39 -0500

    Cleaned up SplashScreen.kt by removing unused imports. The changes include the removal of `Offset`, `Brush`, and `Color` from the file.

--- app/src/main/java/com/example/justcreepinapp/SplashScreen.kt
+++ app/src/main/java/com/example/justcreepinapp/SplashScreen.kt
@@ -13,9 +13,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
