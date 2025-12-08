90a6e5c69d36b9e1edfa25b826de19b1da1907b8
Justin <blizzorbsorc@gmail.com>
2025-12-07 22:22:31 -0500

    feat: Add dynamic status bar to SplashScreen
    This commit updates the `SplashScreen` to dynamically adjust the status bar color and icon theme based on the system's dark or light mode.
    
    - A `SideEffect` is used to change the status bar icon appearance to be light or dark.
    - The background now uses a predefined gradient from `AppTheme.gradient`.

--- app/src/main/java/com/example/justcreepinapp/SplashScreen.kt
+++ app/src/main/java/com/example/justcreepinapp/SplashScreen.kt
@@ -1,25 +1,45 @@
 package com.example.justcreepinapp
 
+import android.app.Activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import com.example.justcreepinapp.ui.theme.AppTheme
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(onTimeout: () -> Unit) {
+
+    // This SideEffect will run when the composable is first displayed
+    val view = LocalView.current
+    val darkTheme = isSystemInDarkTheme()
+    SideEffect {
+        val window = (view.context as Activity).window
+        // Set the status bar color to match the background
+        //window.statusBarColor = MaterialTheme.colorScheme.background.toArgb()
+        // Set the status bar icons to be light or dark
+        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+    }
+
     LaunchedEffect(true) {
         delay(3000)
         onTimeout()
@@ -39,7 +59,7 @@ fun SplashScreen(onTimeout: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(brush = gradient),
+            .background(brush = AppTheme.gradient),
         contentAlignment = Alignment.Center
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
